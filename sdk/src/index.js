dc7701a950f5da664bbfc91a06586690105b598c
nullxnothing <dspivack2@student.cccs.edu>
2026-05-10 17:44:54 -0600

    Export v2 middleware and budget helpers

--- sdk/src/index.js
+++ sdk/src/index.js
@@ -1,2 +1,10 @@
 export { MeterflowClient } from './client.js';
 export { parseSSEStream, parseMultiSSEStream } from './streaming.js';
+export {
+  BUDGET_TEMPLATES,
+  buildBudgetFromTemplate,
+  getBudgetTemplate,
+  listBudgetTemplates,
+  simulateBudget,
+} from './budget-templates.js';
+export { meterflowPaywall, registerMeterflowRoute } from './express.js';
