ba333f784d10fcef8182f3e465c5b2a04032cdd6
nullxnothing <dspivack2@student.cccs.edu>
2026-05-10 18:39:12 -0600

    Export v2 SDK helpers

--- sdk/src/index.js
+++ sdk/src/index.js
@@ -1,2 +1,17 @@
 export { MeterflowClient } from './client.js';
 export { parseSSEStream, parseMultiSSEStream } from './streaming.js';
+export {
+  BUDGET_TEMPLATES,
+  buildBudgetFromTemplate,
+  getBudgetTemplate,
+  listBudgetTemplates,
+  simulateBudget,
+} from './budget-templates.js';
+export { meterflowPaywall, registerMeterflowRoute } from './express.js';
+export {
+  expressMeterflowPaywall,
+  fastifyMeterflowPaywall,
+  fetchMeterflowPaywall,
+  honoMeterflowPaywall,
+  nextMeterflowPaywall,
+} from './adapters.js';
